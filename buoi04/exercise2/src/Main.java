66c9a059804c1a34ed1fa0cb7c06adb2f7205831
nguyenvanquang97 <quangdtkc@gmail.com>
2022-06-08 09:53:43 +0700

    8/6

--- buoi04/exercise2/src/Main.java
+++ buoi04/exercise2/src/Main.java
@@ -4,16 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] numbers;
-        System.out.println("nhập số phần tử của mảng");
+        System.out.println("Nhập số phần tử của mảng");
        int n = sc.nextInt();
         numbers = new int[n];
         for (int i = 0;i<n;i++){
-            System.out.println("nhập phần tử thứ "+(i+1) +":");
+            System.out.println("Nhập phần tử thứ "+(i+1) +":");
             numbers [i]=sc.nextInt();
             if (numbers[i]%2==0){
                 numbers[i]+=1;
             }
+
         }
+        System.out.print("Mảng của bạn là: ");
         for (int j = 0;j<n;j++){
             System.out.print(numbers[j]+"\t");
         }
